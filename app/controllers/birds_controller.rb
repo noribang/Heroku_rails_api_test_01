fa19b74f4a5a08185c55afbe8b15f528832590a5
hoonkim <hoonk9387@gmail.com>
2022-09-14 21:17:41 -0500

    Add show method.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,7 +2,14 @@ class BirdsController < ApplicationController
     # GET /birds
     def index
         birds = Bird.all
-        render json: birds
+        render json: birds  
+    end
+
+    def show
+        bird = Bird.find(params[:id])
+        render json: bird
+    rescue ActiveRecord::RecordNotFound
+        render json: "Bird not found", status: :not_found
     end
 
 end
